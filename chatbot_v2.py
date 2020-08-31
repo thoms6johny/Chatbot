70dc960a105fabfc822d479571bc334885ff2ee5
Thoms Ampalore Johny (RBEI/EEP22-EC) <tjh4kor@bosch.com>
2020-08-31 12:44:53 +0530

    First Commit

---
+++ chatbot_v2.py
@@ -0,0 +1,114 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy as np
+import tensorflow as tf
+import random
+import json
+import pickle
+from tensorflow import keras
+
+with open("intents.json") as file:
+    data = json.load(file)
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words =[]
+    labels= []
+    docs_x = []
+    docs_y = []
+
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [stemmer.stem(w) for w in doc]
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+
+    training = np.array(training)
+    output = np.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training,output), f)
+
+
+model = keras.Sequential([
+    keras.layers.Input(shape = (len(training[0]))),
+    keras.layers.Dense(8),
+    keras.layers.Dense(len(output[0]), activation = "softmax")
+])
+
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+
+try:
+
+    model = keras.models.load_model("Model.h5")
+except:
+    model.fit(training, output, epochs=1000, batch_size=8)
+    model.save("Model.h5")
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i,w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+    return bag
+
+def chat():
+    print("start talking with the bot! ")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
+
+        results = model.predict([bag_of_words(inp, words)])[0]
+        results_index = np.argmax(results)
+        tag = labels[results_index]
+
+        if results[results_index]>0.7:
+            for tg in data["intents"]:
+                if tg["tag"] == tag:
+                    responses = tg['responses']
+            print(random.choice(responses))
+
+        else:
+            print("I didn't get that.")
+
+chat()
